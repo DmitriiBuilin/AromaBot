7773c40e3a8d67c3d3fd83a79fe93901b4d79fe5
Dmitrii Builin <d.builin@mail.ru>
2023-05-21 20:33:48 +0300

    Add button. Non actions

--- src/main/java/tutorial/Bot.java
+++ src/main/java/tutorial/Bot.java
@@ -1,11 +1,19 @@
 package tutorial;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.CopyMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 
+
+import java.util.List;
 import java.util.Map;
+
 
 public class Bot extends TelegramLongPollingBot {
     private static final Map<String, String> getenv = System.getenv();
@@ -27,9 +35,58 @@ public class Bot extends TelegramLongPollingBot {
 
         var id = user.getId();
 
-        sendText(id, msg.getText()); // echo user
+        var next = InlineKeyboardButton.builder() // Button 1
+                .text("Next ➡\uFE0F").callbackData("next")
+                .build();
+
+        var back = InlineKeyboardButton.builder() // Button 2
+                .text("Back ⬅\uFE0F").callbackData("back")
+                .build();
+
+        var url = InlineKeyboardButton.builder()
+                .text("Web-site \uD83D\uDDA5")
+                .url("http://project7291932.tilda.ws/")
+                .build();
+
+        var copy = InlineKeyboardButton.builder()
+                .text("Copy \uD83D\uDCD1")
+                .callbackData("copy")
+                .build();
+
+        InlineKeyboardMarkup keyboard_1; // Initialize Keyboard
+        InlineKeyboardMarkup keyboard_2;
+        keyboard_1 = InlineKeyboardMarkup.builder()
+                .keyboardRow(List.of(copy))
+                .keyboardRow(List.of(next)).build();
+        keyboard_2 = InlineKeyboardMarkup.builder()
+                .keyboardRow(List.of(url))
+                .keyboardRow(List.of(back))
+                .build();
+
+
+//        copyMessage(id, update.getMessage().getMessageId());   // Echo Bot
         System.out.println(user.getUserName() + " wrote " + msg.getText());
+
+        if (update.hasCallbackQuery()) {
+            String callbackData = update.getCallbackQuery().getData();
+            // Handle the callback data and perform the desired action
+            if (callbackData.equals("your_button_callback_data")) {
+                // Button with "your_button_callback_data" was clicked, perform action
+                // ...
+            }
+        }
+
+
+        if(msg.isCommand()){
+            if(msg.getText().equals("/start"))
+                sendMenu(id, "<b>Выберите действие \uD83D\uDE0E</b>", keyboard_1);
+            else if (msg.getText().equals("/help"))
+                sendMenu(id, "<b>Выберите действие \uD83D\uDE0E</b>", keyboard_2);
+
+            return;            //We don't want to echo commands, so we exit
+        }
     }
+
 
     public void sendText(Long who, String what){
         SendMessage sm = SendMessage.builder()
@@ -39,6 +96,44 @@ public class Bot extends TelegramLongPollingBot {
             execute(sm);                        //Actually sending the message
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);      //Any error will be printed here
+        }
+    }
+
+
+    public void copyMessage(Long who, Integer msgId){
+        CopyMessage cm = CopyMessage.builder()
+                .fromChatId(who.toString())  //We copy from the user
+                .chatId(who.toString())      //And send it back to him
+                .messageId(msgId)            //Specifying what message
+                .build();
+        try {
+            execute(cm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void sendMenu(Long who, String txt, InlineKeyboardMarkup kb){
+        SendMessage sm = SendMessage.builder().chatId(who.toString())
+                .parseMode("HTML").text(txt)
+                .replyMarkup(kb).build();
+
+        try {
+            execute(sm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void clickButton(String chatId, int messageId) {
+        EditMessageReplyMarkup editMessageReplyMarkup = new EditMessageReplyMarkup()
+                .setChatId(chatId)
+                .setMessageId(messageId)
+                .setReplyMarkup(null);  // Set the new markup or null to remove the buttons
+        try {
+            execute(editMessageReplyMarkup);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
     }
 }
